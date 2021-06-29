fefa3e917fce3d39b9cdf83e3d0bed44d1e40b07
Ali Khurram <ak@adeogroup.co.uk>
2021-06-29 10:25:06 +0500

    User Register Form Done

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Login = ()=>{
-    return(
-        <div className="contianer ">
-            <div className="row p-5">
-                <h4>
-                    Login
-                </h4>
-            </div>
-        </div>
-    );
-}
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [loading, setloading] = useState(false);
+  function handleSubmit() {
+    //Will be implemented later
+  }
+  return (
+    <div className="contianer ">
+      <div className="row p-5">
+        <h4>Login</h4>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Email Address</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              placeholder="Enter email"
+              disabled={loading}
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React,{useState} from "react";
 
-const Register = ()=>{
-    return(
-        <div className="contianer ">
-            <div className="row p-5">
-                <h4>
-                    Register
-                </h4>
-            </div>
-        </div>
-    );
-}
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [loading, setloading] = useState(false);
+  function handleSubmit() {
+    //Will be implemented later
+  }
+  return (
+    <div className="contianer ">
+      <div className="row p-5">
+        <h4>Register</h4>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Email Address</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              placeholder="Enter email"
+              disabled={loading}
+            />
+          </div>
+          <button className="btn btn-raised btn-primary mt-2" disabled={!email || loading}>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Register;
