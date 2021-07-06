b4d44ee51464a73f25dd4bfb9e4c623c32949c93
AwaisSikander <sikande007@gmail.com>
2021-07-06 11:53:35 +0500

    Google Login Implemented

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -7,9 +7,9 @@ const Login = () => {
   const [email, setEmail] = useState("sikande007@gmail.com");
   const [password, setPassword] = useState("Awais@786");
   const [loading, setloading] = useState(false);
-  const [success, setSuccess] = useState(false);
   const { dispatch } = useContext(AuthContext);
   let history = useHistory();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setloading(true);
@@ -33,9 +33,24 @@ const Login = () => {
       setloading(false);
     }
   };
+  const googleLogin = () => {
+    auth.signInWithPopup(googleAuthProvider).then(async (result) => {
+      const { user } = result;
+      const idTokenResult = await user.getIdTokenResult();
+      dispatch({
+        type: "LOGGED_IN_USER",
+        payload: { email: user.email, token: idTokenResult.token },
+      });
+      // send user info to our server to save/update in mongodb
+      // Then Redirect
+      history.push("/");
+    });
+  };
+
   return (
     <div className="contianer  p-5">
-      <h4>Login</h4>
+      {loading ? <h4 className="text-danger">Loading...</h4> : <h4>Login</h4>}
+      <button onClick={googleLogin} className="btn btn-danger my-3">Login with google</button>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>Email Address</label>
