5d634db2eb887a6296c84277f927b752d210aac7
AwaisSikander <sikande007@gmail.com>
2021-07-06 11:32:37 +0500

    34. Login with firebase

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, Fragment } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { auth } from "../firebase";
 import { AuthContext } from "../context/authContext";
@@ -33,16 +33,24 @@ const Nav = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/login">
-                Login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="register">
-                Register
-              </Link>
-            </li>
+            {!user && (
+              <Fragment>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link active"
+                    aria-current="page"
+                    to="/login"
+                  >
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="register">
+                    Register
+                  </Link>
+                </li>
+              </Fragment>
+            )}
             {user && (
               <li className="nav-item">
                 <a href="/login" onClick={logout} className="nav-item nav-link">
--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,29 +1,71 @@
-import React, { useState } from "react";
-
+import React, { useState, useContext } from "react";
+import { AuthContext } from "../../context/authContext";
+import { Link, useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
+import { auth, googleAuthProvider } from "../../firebase";
 const Login = () => {
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState("sikande007@gmail.com");
+  const [password, setPassword] = useState("Awais@786");
   const [loading, setloading] = useState(false);
-  function handleSubmit() {
-    //Will be implemented later
-  }
+  const [success, setSuccess] = useState(false);
+  const { dispatch } = useContext(AuthContext);
+  let history = useHistory();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setloading(true);
+    try {
+      await auth
+        .signInWithEmailAndPassword(email, password)
+        .then(async (result) => {
+          const { user } = result;
+          const idTokenResult = await user.getIdTokenResult();
+          dispatch({
+            type: "LOGGED_IN_USER",
+            payload: { email: user.email, token: idTokenResult.token },
+          });
+          // send user info to our server to save/update in mongodb
+          // Then Redirect
+          history.push("/");
+        });
+    } catch (error) {
+      console.log("Login Error", error);
+      toast.error(error.message);
+      setloading(false);
+    }
+  };
   return (
-    <div className="contianer ">
-      <div className="row p-5">
-        <h4>Login</h4>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label>Email Address</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="form-control"
-              placeholder="Enter email"
-              disabled={loading}
-            />
-          </div>
-        </form>
-      </div>
+    <div className="contianer  p-5">
+      <h4>Login</h4>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+            placeholder="Enter email"
+            disabled={loading}
+          />
+        </div>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control"
+            placeholder="Enter password"
+            disabled={loading}
+          />
+        </div>
+        <button
+          className="btn btn-raised btn-primary mt-2"
+          disabled={!email || !password || loading}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
