8ebf5d1b66121c450b163254abae7ff7a2094ff3
AwaisSIkander <sikande007@gmail.com>
2021-07-26 12:43:05 +0500

    lec 56 TYPE DEF COMPRESS

--- server/typeDefs/auth.js
+++ server/typeDefs/auth.js
@@ -4,4 +4,12 @@ module.exports = gql`
     type Query {
         me: String!
     }
+    #CUSTOM TYPE
+    type UserCreateResponse{
+        username:String!
+        email:String!
+    }
+    type Mutation {
+        userCreate:UserCreateResponse!
+    }
 `
